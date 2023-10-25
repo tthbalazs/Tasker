c484f9eb7e3cf78477826b5abc115769f728d640
MaToSens <matoosens@gmail.com>
2023-10-25 13:17:14 +0200

    Add initial files

---
+++ Tasker/Tasker/TaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskerApp.swift
+//  Tasker
+//
+//  Created by MaTooSens on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
